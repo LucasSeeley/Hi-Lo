8e7c837c2b2d53388aec225155484faaddfac795
LucasSeeley <lucas.a.seeley@gmail.com>
2020-08-17 10:52:26 -0400

    initial commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) {
+		Scanner t = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String r = t.nextLine();
+		System.out.println("Hello " + r);
+
+	}
+
+}
