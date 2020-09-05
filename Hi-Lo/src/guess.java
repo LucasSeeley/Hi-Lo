d962db649f99df81f4cbd04bbe9a14c587b4a93b
LucasSeeley <lucas.a.seeley@gmail.com>
2020-09-05 16:56:46 -0400

    Initial Commit

---
+++ Hi-Lo/src/guess.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class guess {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hi-Lo" + '\n' + "The highest possible score is 100");
+		System.out.println("Guess a number 0 to 100!");
+		
+		
+		Scanner user = new Scanner(System.in);
+		String y = user.nextLine();
+		int z = Integer.parseInt(y);
+		
+		
+		Random rand = new Random();
+		int x = rand.nextInt(101);
+		
+		
+		int score = 100;
+		
+		
+		while (x != z) {
+			if (score == 0) {
+				System.out.println("You have incorrectly guessed too many times");
+				break;
+			}
+			else if (x > z) {
+				System.out.println("Guess higher");
+				y = user.nextLine();
+				z = Integer.parseInt(y);
+				score = score - 10;
+				continue;
+			}
+			else if (x < z) {
+				System.out.println("Guess lower");
+				y = user.nextLine();
+				z = Integer.parseInt(y);
+				score = score - 10;
+				continue;
+			}
+			else {
+
+			}
+				
+		}
+		
+		System.out.println("Great Job!" + '\n' + "Game Over" + '\n' + "Your score is " + score);
+		
+	}
+
+}
